0f983f29d1277acb368e536f44ebb1a8eb625903
Mirraim <mirraim@gmail.com>
2021-04-29 20:33:45 +0500

    технический коммит

--- src/test/java/ru/job4j/quartz/AlertRabbitTest.java
+++ src/test/java/ru/job4j/quartz/AlertRabbitTest.java
@@ -1,14 +1,5 @@
 package ru.job4j.quartz;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 public class AlertRabbitTest {
 
-    @Test
-    public void whenGetInterval() {
-        int rsl = AlertRabbit.getInterval("rabbit.properties");
-        assertEquals(rsl, 10);
-    }
 }
